8b3dbc0bcf81d195498628cf3acfa1dbdfa558a7
Carlos Silva <carlos.henrique@bs2.com>
2019-11-22 18:16:42 -0300

    Implementando retorno de cursos.

--- src/CursoOnline.Web/Controllers/CursoController.cs
+++ src/CursoOnline.Web/Controllers/CursoController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using CurosOnline.Dominio._Base;
 using CursoOnline.Cursos;
 using CursoOnline.Dominio.Cursos;
 using CursoOnline.Web.Util;
@@ -9,16 +11,32 @@ namespace CursoOnline.Web.Controllers
     public class CursoController : Controller
     {
         private readonly ArmazenadorDeCurso _armazenadorDeCurso;
-        
-        public CursoController(ArmazenadorDeCurso armazenadorDeCurso)
+        private readonly IRepositorio<Curso> _cursoRepositorio;
+
+
+        public CursoController(ArmazenadorDeCurso armazenadorDeCurso, IRepositorio<Curso> curso)
         {
             _armazenadorDeCurso = armazenadorDeCurso;
+            _cursoRepositorio = curso;
         }
         
         public IActionResult Index()
         {
-            var cursos = new List<CursoParaListagemDto>();
-            return View("Index", PaginatedList<CursoParaListagemDto>.Create(cursos, Request));
+            var cursos = _cursoRepositorio.Consultar();
+
+            if (cursos.Any())
+            {
+                var CursosDtos = cursos.Select(x => new CursoParaListagemDto
+                {
+                    Id = x.Id,
+                    CargaHoraria = x.CargaHoraria,
+                    Nome = x.Nome,
+                    PublicoAlvo = x.PublicoAlvo.ToString(),
+                    Valor = x.Valor
+                });
+                return View("Index", PaginatedList<CursoParaListagemDto>.Create(CursosDtos, Request));
+            }
+            return View("Index", PaginatedList<CursoParaListagemDto>.Create(null, Request));
         }
         
         public IActionResult Novo()
