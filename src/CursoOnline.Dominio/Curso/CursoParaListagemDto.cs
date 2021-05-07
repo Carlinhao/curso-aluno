9c3f0e3b2071f56eb8be04a2d128b20311b3589a
Carlinhao <carsilva.ti@gmail.com>
2021-05-06 23:29:16 -0300

    Altearndo properties double para decimal

--- src/CursoOnline.Dominio/Curso/CursoParaListagemDto.cs
+++ src/CursoOnline.Dominio/Curso/CursoParaListagemDto.cs
@@ -6,6 +6,6 @@
         public string Nome { get; set; }
         public double CargaHoraria { get; set; }
         public string PublicoAlvo { get; set; }
-        public double Valor { get; set; }
+        public decimal Valor { get; set; }
     }
 }
