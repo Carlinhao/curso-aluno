d2f20c039baf8b15a1248f92033283b042501adb
Carlinhao <carsilva.ti@gmail.com>
2021-05-06 23:39:50 -0300

    Refatorando MatriculaController

--- src/CursoOnline.Web/Controllers/MatriculaController.cs
+++ src/CursoOnline.Web/Controllers/MatriculaController.cs
@@ -1,22 +1,80 @@
 ﻿using System;
+using System.Linq;
+using CurosOnline.Dominio._Base;
+using CurosOnline.Dominio.Alunos;
+using CurosOnline.Dominio.Matriculas;
+using CursoOnline.Cursos;
+using CursoOnline.Dominio.Matriculas;
+using CursoOnline.Web.Util;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CursoOnline.Web.Controllers
 {
     public class MatriculaController : Controller
     {
-        public MatriculaController()
+        private readonly IMatriculaRepositorio _matriculaRepositorio;
+        private readonly IRepositorio<Aluno> _alunoRepositorio;
+        private readonly IRepositorio<Curso> _cursoRepositorio;
+        private readonly CriacaoDaMatricula CriacaoDaMatricula;
+
+        public MatriculaController(
+            IMatriculaRepositorio matriculaRepositorio,
+            IRepositorio<Aluno> alunoRepositorio,
+            IRepositorio<Curso> cursoRepositorio,
+            CriacaoDaMatricula criacaoDaMatricula)
         {
+            _matriculaRepositorio = matriculaRepositorio;
+            _alunoRepositorio = alunoRepositorio;
+            _cursoRepositorio = cursoRepositorio;
+            CriacaoDaMatricula = criacaoDaMatricula;
         }
 
         public IActionResult Index()
         {
-            throw new InvalidOperationException();
+            var matriculas = _matriculaRepositorio.Consultar();
+
+            if (matriculas.Any())
+            {
+                var dtos = matriculas.Where(m => !m.Cancelada).Select(c => new MatriculaParaListagemDto
+                {
+                    Id = c.Id,
+                    NomeDoAluno = c.Aluno.Nome,
+                    NomeDoCurso = c.Curso.Nome,
+                    Valor = c.ValorPago
+                });
+                return View("Index", PaginatedList<MatriculaParaListagemDto>.Create(dtos, Request));
+            }
+
+            return View("Index", PaginatedList<MatriculaParaListagemDto>.Create(null, Request));
         }
-        
-        public IActionResult Salvar()
+
+        public IActionResult Nova()
         {
-            throw new InvalidOperationException();
+            InicializarAlunosECursosNaViewBag();
+
+            return View("Nova", new MatriculaDto());
+        }
+
+        private void InicializarAlunosECursosNaViewBag()
+        {
+            var alunos = _alunoRepositorio.Consultar();
+            if (alunos != null && alunos.Any())
+                alunos = alunos.OrderBy(a => a.Nome).ToList();
+
+            ViewBag.Alunos = alunos;
+
+            var cursos = _cursoRepositorio.Consultar();
+            if (cursos != null && cursos.Any())
+                cursos = cursos.OrderBy(c => c.Nome).ToList();
+
+            ViewBag.Cursos = cursos;
+        }
+
+        [HttpPost]
+        public IActionResult Salvar(MatriculaDto model)
+        {
+            CriacaoDaMatricula.Criar(model);
+            return Ok();
         }
     }
 }
