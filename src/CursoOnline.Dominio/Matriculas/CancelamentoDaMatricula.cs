85fc5d29b19ac76522318213a3433210b24d4d37
Carlinhao <carsilva.ti@gmail.com>
2021-05-06 23:49:11 -0300

    Criando metod cancelar matricula

---
+++ src/CursoOnline.Dominio/Matriculas/CancelamentoDaMatricula.cs
@@ -0,0 +1,26 @@
+﻿using CurosOnline.Dominio;
+using CurosOnline.Dominio._Base;
+
+namespace CursoOnline.Dominio.Matriculas
+{
+    public class CancelamentoDaMatricula
+    {
+        private readonly IMatriculaRepositorio _matriculaRepositorio;
+
+        public CancelamentoDaMatricula(IMatriculaRepositorio matriculaRepositorio)
+        {
+            _matriculaRepositorio = matriculaRepositorio;
+        }
+
+        public void Cancelar(int matriculaId)
+        {
+            var matricula = _matriculaRepositorio.ObterPorId(matriculaId);
+
+            ValidadorDeRegra.Novo()
+                .Quando(matricula == null, Resource.MatriculaNaoEncontrada)
+                .DispararExcecaoSeExistir();
+
+            matricula.Cancelar();
+        }
+    }
+}
