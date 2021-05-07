3d3c8b83b08e3ae36b7d5fcbfeb7bf668cd73594
Carlinhao <carsilva.ti@gmail.com>
2021-05-06 23:32:09 -0300

    Removendo contrutor

--- src/CursoOnline.Dominio/Matriculas/MatriculaDto.cs
+++ src/CursoOnline.Dominio/Matriculas/MatriculaDto.cs
@@ -2,13 +2,6 @@
 {
     public class MatriculaDto
     {
-        public MatriculaDto(int alunoId, int cursoId, decimal valorPago)
-        {
-            AlunoId = alunoId;
-            CursoId = cursoId;
-            ValorPago = valorPago;
-        }
-
         public int AlunoId { get; set; }
         public int CursoId { get; set; }
         public decimal ValorPago { get; set; }
