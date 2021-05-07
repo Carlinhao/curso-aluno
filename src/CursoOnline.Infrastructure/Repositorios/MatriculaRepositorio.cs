172b8433ae1d93786c071054921e4e8033dc2d12
Carlinhao <carsilva.ti@gmail.com>
2021-05-07 20:45:17 -0300

    Criação do repositorio de Matricula

---
+++ src/CursoOnline.Infrastructure/Repositorios/MatriculaRepositorio.cs
@@ -0,0 +1,25 @@
+﻿using CursoOnline.Dominio.Matriculas;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoOnline.Infrastructure.Repositorios
+{
+    public class MatriculaRepositorio : IMatriculaRepositorio
+    {
+        public void Adicionar(MatriculaDomain entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MatriculaDomain> Consultar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MatriculaDomain ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
